6e1b0c408f3678960b65db90b65cd0e8f6fbc064
jefferyrayrussell <jefferyrayrussell@gmail.com>
2016-03-30 07:50:35 -0700

    Initial Organization of necessary constructors, prototypes, functions, and variables

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 console.log('javascript works');
 
+//global variables to name the images array and set counters to zero.
+var productImagesArray = [];
+var numberTimesDisplayed = 0;
+var numberTimesSelected = 0;
+var percentageTimeSelected = 0;
+
+//function calculating percentageTimesSelected
+var percentageTimesSelected = function percentage(numberTimesSelected, numberTimesDisplayed){
+  Math.round(numberTimesSelected / numberTimesDisplayed * 100);
+};
+
+//function used to randomly select imageSource by generating a random integer
+//between two numbers low inclusive and high inclusive.
+function getRandomIntegerInclusive(min,max){
+  return Mathfloor (Math.random() * (max - min + 1) + min);
+}
+
 /*The object constructor identified as SalesItem creates objects that are instances
 of salesItem and contain the properties of : name, imageSource, numberTimesDisplayed, numberTimesSelected, percentageTimesSelected.*/
 
@@ -11,22 +28,17 @@ function SalesItem(name) {
   this.percentageTimesSelected = percentageTimesSelected;
 }
 
-/*The object constructor SalesItem is given a method that takes an image found in
+/* The object constructor SalesItem is given a method that takes an image found in
 a file and places that image on a web page. The variable imageElement imgEl is
-created.  The variable is given content.  The element is placed on the page.
+created.  The variable is given content.  The element is placed on the page.*/
 
-*/
 SalesItem.prototype.renderToPage = function(){
-  imgEl = document.createElement("img");
+  imgEl = document.createElement('img');
   imgEl.setAttribute(imageSource, this.imageSource);
   document.appendChild();
-  // place image on the page
 };
 
-
-var percentageTimesSelected = function percentage(numberTimesSelected, numberTimesDisplayed){
-  Math.round(numberTimesSelected / numberTimesDisplayed * 100);
-};
+//function to handle image click event
 
 function handleImageClick(event){
   console.log('event.target: ', event.target);
