8f0c21d6e988e6c54db889e935c2b8f3e21b3efb
jefferyrayrussell <jefferyrayrussell@gmail.com>
2016-03-30 20:15:39 -0700

    Images on Page Loading Randomly

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ console.log('javascript works');
 var testProductImagesArray = [];
 var displayTestProductImagesArray = [];
 
+var totalGlobalNumberDisplayed = 0;
+var totalGlobalNumberSelected = 0;
+
 var randomDisplayImageLeft;
 var randomDisplayImageMiddle;
 var randomDisplayImageRight;
@@ -21,6 +24,7 @@ function TestProduct(testProductName, filePath) {
   this.numberTimesSelected = 0;
   testProductImagesArray.push(this);
 }
+
 /* Twenty TestProduct objects are created using the TestProduct constructor above
 and the arguments provided below. Each instance of the constructor is stored in
 a variable. */
@@ -41,11 +45,11 @@ var pen = new TestProduct('Utensil Pens', 'img/pen.jpg');
 var petsweep = new TestProduct('Pet Floor Dusters', 'img/petsweep.jpg');
 var scissors = new TestProduct('Pizza Scissors', 'img/scissors.jpg');
 var shark = new TestProduct('Shark Sleeping Bag', 'img/shark.jpg');
-var sweep = new TestProduct('Baby Floor Sweeper', 'img/sweep.jpg');
+var sweep = new TestProduct('Baby Floor Sweeper', 'img/sweep.png');
 
 var tauntaun = new TestProduct('Dog Sleeping Bag', 'img/tauntaun.jpg');
 var unicorn = new TestProduct('Unicorn Meat', 'img/unicorn.jpg');
-var usb = new TestProduct('Tentacle USB', 'img/usb.jpg');
+var usb = new TestProduct('Tentacle USB', 'img/usb.gif');
 var watercan = new TestProduct('Watering Can', 'img/waterCan.jpg');
 var wineglass = new TestProduct('Blown Wine Glass', 'img/wineGlass.jpg');
 
@@ -54,54 +58,50 @@ console.log(testProductImagesArray);
 /*The generateRandomImagesFunction is used to access the testProductImagesArray
 and select three random images to be displayed on the page.*/
 
-generateRandomImagesFunction = function() {
+generateRandomImagesIndexFunction = function() {
   return Math.floor(Math.random() * testProductImagesArray.length);
 };
 
 /* The renderRandomImages function places three random product images stored in
 three variables on the page. First, the function gets an image element with a
-certain Id on the html page so that it can be given content.  Second, the function  adds
-content to that image element by randomly assigning it an image from the
+certain Id on the html page so that it can be given content.  Second, the function
+adds content to that image element by randomly assigning it an image from the
 testProductImagesArray.*/
 
 var renderRandomImagesFunction = function(){
 
-  /*First, the function gets an image element with a certain Id on the html page
-  so that it can be given content.*/
+/*First, the function gets three image elements with a certain Id on the html page
+so that it can be given content. Second, the function adds content to each
+image element by randomly assigning it an image from the testProductImagesArray.
+It does so by using the generateRandomImagesFunction to get a random number to
+place in the array and to randomly call forth an image by means of it's file
+path. In addition, the number of times the image is displayed is also stored
+back in the object property numberTimesDisplayed. The while statements insure
+that there are not duplicate pictures on the display*/
 
   var randomDisplayImageLeft = document.getElementById('randomDisplayImageLeft');
   var randomDisplayImageMiddle = document.getElementById('randomDisplayImageMiddle');
   var randomDisplayImageRight = document.getElementById('randomDisplayImageRight');
 
-  /* Second, the function adds content to each image element by randomly
-  assigning it an image from the testProductImagesArray.  It does so by using
-  the generateRandomImagesFunction to get a random number to place in the array
-  and to randomly call forth an image by means of it's file path. In addition,
-  the number of times the image is displayed is also stored back in the object property
-  numberTimesDisplayed. */
+  imageIndex1 = generateRandomImagesIndexFunction();
+  randomDisplayImageLeft.src = testProductImagesArray[imageIndex1].filePath;
+  testProductImagesArray[imageIndex1].numberTimesDisplayed ++;
 
-  image1 = generateRandomImagesFunction();
-  randomDisplayImageLeft.src = testProductImagesArray[image1].filePath;
-  testProductImagesArray[image1].numberTimesDisplayed ++;
+  imageIndex2 = generateRandomImagesIndexFunction();
+  while (imageIndex1 === imageIndex2) {
+    imageIndex2 = generateRandomImagesIndexFunction();
+  }
+  randomDisplayImageMiddle.src = testProductImagesArray[imageIndex2].filePath;
+  testProductImagesArray[imageIndex2].numberTimesDisplayed ++;
 
-  image2 = generateRandomImagesFunction();
-  randomDisplayImageMiddle.src = testProductImagesArray[img2].filePath;
-  testProductImagesArray[image2].numberTimesDisplayed ++;
-
-  image3 = generateRandomImagesFunction();
-  randomDisplayImageRight.src = testProductImagesArray[img3].filePath;
-  testProductImagesArray[img3].numberTimesDisplayed ++;
+  imageIndex3 = generateRandomImagesIndexFunction();
+  while (imageIndex1 === imageIndex2 || imageIndex2 === imageIndex3 || imageIndex3 === imageIndex1) {
+    imageIndex3 = generateRandomImagesIndexFunction();
+  }
+  randomDisplayImageRight.src = testProductImagesArray[imageIndex3].filePath;
+  testProductImagesArray[imageIndex3].numberTimesDisplayed ++;
 };
-
-
-
-//reselect a random product image if any of the three pictures are duplicates.
-
-//if (productSelector.leftObject === productSelector.middleObject || productSelector.rightObject === productSelector.rightImage || productSelector.rightObject === productSelector.middleObject)
-//{productSelector.getRandomProductImageFunction();}
-
-
-
+renderRandomImagesFunction();
 /* The object constructor TestProduct is given a method that takes an image found in
 a file and places that image on a web page. The variable imageElement imgEl is
 created.  The variable is given content.  The element is placed on the page.*/
